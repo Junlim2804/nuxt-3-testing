937609c652589dc42f4cc685c00b73012cae6c6e
TJ <junlim2804@gmail.com>
2022-09-02 02:02:39 +0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        This is nuxt 3 testing
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+</script>
